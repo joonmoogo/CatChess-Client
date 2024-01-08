216c9c80968b7950e48c6f22906aa9ba541571c7
joonmoogo <junemuk10@gmail.com>
2024-01-08 16:48:39 +0900

    add : money style

--- src/Components/BottomSide/Money.js
+++ src/Components/BottomSide/Money.js
@@ -1,14 +1,29 @@
-export default function Money({money}) {
-    const buttonStyle={
-        color:'white',
-        border:'1px solid white',
-        height:'50%'
+export default function Money({ money }) {
+    const buttonStyle = {
+        color: 'white',
+        borderBottom: '25px solid white',
+        borderLeft: '10px solid transparent',
+        borderRight: '10px solid transparent',
+        height: '0%',
+        width:'50px',
+        
     }
 
-    function reloadHandler(event){
+    const textStyle = {
+        textAlign:'center',
+        position:'absolute',
+        color:'black'
+    }
+
+    function reloadHandler(event) {
         console.log('Reload button was clicked');
     }
+
     return (
-        <div style={buttonStyle} onClick={reloadHandler}>💽 50</div>
+        <>
+            <div style={buttonStyle} onClick={reloadHandler}>
+            <div style={textStyle}>💽{money}100</div>
+            </div>
+        </>
     )
 }
